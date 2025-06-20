5488caccdb57bc0be204388ca6d811d2c2ec88c4
solotoxicxd <solotoxicxd@gmail.com>
2025-06-20 19:22:55 +0530

    Update boardstate.js

--- boardstate.js
+++ boardstate.js
@@ -1,88 +1,82 @@
 class BoardState {
-  constructor(state, goal_state, size, path = " ", path_states, depth) {
+  constructor(state, goal_state, size, path = "", path_states = [], depth = 0) {
     this.state = state;
     this.goal_state = goal_state;
-    this.value = 0;
     this.size = size;
+
     this.path = path;
     this.path_states = path_states;
     this.depth = depth;
+
+    this.value = 0;
     this.empty_tile_row = null;
     this.empty_tile_col = null;
+
     this.setEmptyTilePos();
     this.setValue();
     this.addPathState();
   }
 
+  // Returns the move path (e.g., "ULDR")
   getPath() {
-    /**
-     * get the path from state to goal state
-     */
-
     return this.path;
   }
 
+  // Calculates and sets the heuristic value
   setValue() {
+    // Only include depth for size ≤ 3 to avoid overloading larger puzzles
     this.value = (this.size > 3 ? 0 : this.depth) + this.manhattanDistance();
   }
 
+  // Clones and adds current state to the path trail
   addPathState() {
-    let path_states_clone = JSON.parse(JSON.stringify(this.path_states));
-    path_states_clone.push(this.state);
-    this.path_states = path_states_clone;
+    const clone = JSON.parse(JSON.stringify(this.path_states));
+    clone.push(this.state);
+    this.path_states = clone;
   }
 
-  /**
-   * A misplaced Tiles Heurestics
-   */
+  // Heuristic: Misplaced tiles (not used in A* but useful for testing)
   misplacedTiles() {
-    console.log(this.state, this.goal_state);
-    let result = 0;
+    let count = 0;
     for (let i = 0; i < this.state.length; i++) {
-      for (let j = 0; j < this.state.length; j++) {
-        if (
-          this.state[i][j] != this.goal_state[i][j] &&
-          this.state[i][j] != 0
-        ) {
-          result++;
+      for (let j = 0; j < this.state[i].length; j++) {
+        if (this.state[i][j] !== 0 && this.state[i][j] !== this.goal_state[i][j]) {
+          count++;
         }
       }
     }
-    return result;
+    return count;
   }
 
-  /**
-   * A Manhattan Distance Heurestics
-   */
+  // Heuristic: Manhattan Distance
   manhattanDistance() {
-    var result = 0;
+    let total = 0;
 
-    for (var i = 0; i < this.state.length; i++) {
-      for (var j = 0; j < this.state[i].length; j++) {
-        var elem = this.state[i][j];
-        var found = false;
-        for (var h = 0; h < this.goal_state.length; h++) {
-          for (var k = 0; k < this.goal_state[h].length; k++) {
-            if (this.goal_state[h][k] == elem) {
-              result += Math.abs(h - i) + Math.abs(j - k);
-              found = true;
-              break;
+    for (let i = 0; i < this.state.length; i++) {
+      for (let j = 0; j < this.state[i].length; j++) {
+        const tile = this.state[i][j];
+
+        if (tile !== 0) {
+          for (let x = 0; x < this.goal_state.length; x++) {
+            for (let y = 0; y < this.goal_state[x].length; y++) {
+              if (this.goal_state[x][y] === tile) {
+                total += Math.abs(x - i) + Math.abs(y - j);
+                break;
+              }
             }
           }
-          if (found) break;
         }
       }
     }
-    return result;
+
+    return total;
   }
 
+  // Finds the position of the empty tile (0)
   setEmptyTilePos() {
-    let state = this.state.flatMap((el) => el);
-    state.forEach((el, index) => {
-      if (el == 0) {
-        this.empty_tile_row = Math.floor(index / this.size);
-        this.empty_tile_col = index % this.size;
-      }
-    });
+    const flat = this.state.flat();
+    const index = flat.indexOf(0);
+    this.empty_tile_row = Math.floor(index / this.size);
+    this.empty_tile_col = index % this.size;
   }
 }
